972feb0ef51bb3a2db4f4ede5f1ee49d765d6854
etsploit <etsploit@protonmail.ch>
2022-05-17 22:07:14 -0400

    re-org and all list comparison

---
+++ data-comparison/compare-list.py
@@ -0,0 +1,15 @@
+import difflib
+
+#edit the path
+
+ips1 = "/Users/your_user/sub_folder/input/file1.txt"
+ips2 = "/Users/your_user/sub_folder/input/file2.txt"
+
+ips1_lines = open(ips1, 'r').readlines()
+ips2_lines = open(ips2, 'r').readlines()
+
+difference = difflib.HtmlDiff().make_file(ips1_lines,ips2_lines,'IP Set 
+1','IP Set 2')
+difference_report = open('difference_report.html', 'w')
+difference_report.write(difference)
+difference_report.close()
